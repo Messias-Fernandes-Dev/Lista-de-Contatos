e261d932503c4e659d4fddfa745d4e78c364c686
Messias Fernandes <dev.messiasfer@gmail.com>
2025-08-12 13:31:40 -0300

    criando servidor http com Express

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const app = express();
+
+app.get('/', (request, response) => {
+  response.send('Hello API with Express')
+})
+
+app.listen(3000, () => console.log('✅ Server started at http://localhost:3000'))
