79e4816b88c23b69c536798c2db08c3f19ec0201
Messias Fernandes <dev.messiasfer@gmail.com>
2025-08-15 19:45:51 -0300

    Criando as rotas de show e delete

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,12 +1,19 @@
-const { uuid } = require("uuidv4");
+const { v4 } = require("uuid");
 
-const contacts = [
+let contacts = [
   {
-    id: uuid(),
+    id: v4(),
     name: "Messias",
     email: "messias@gmail.com",
     phone: "233221",
-    category_id: uuid(),
+    category_id: v4(),
+  },
+  {
+    id: v4(),
+    name: "Lawane",
+    email: "law@gmail.com",
+    phone: "998198213",
+    category_id: v4(),
   },
 ];
 
@@ -14,6 +21,19 @@ class ContactsRepository {
   findAll() {
     return new Promise((resolve) => {
       resolve(contacts);
+    });
+  }
+
+  findById(id) {
+    return new Promise((resolve) =>
+      resolve(contacts.find((contact) => contact.id === id))
+    );
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve()
     });
   }
 }
