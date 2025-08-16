c5e5ef9738a97a27a17f706dfde005ec4f783d76
Messias Fernandes <dev.messiasfer@gmail.com>
2025-08-16 19:03:50 -0300

    Criando a rota de edição de Contatos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
+const routes = require("./routes");
+
 const app = express();
 
-const routes = require('./routes')
-app.use(routes)
+app.use(express.json())
+app.use(routes);
 
 app.listen(3000, () =>
   console.log("✅ Server started at http://localhost:3000")
