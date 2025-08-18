69b8cc82f9f265df4993873556ecc4d2d13d230a
Messias Fernandes <dev.messiasfer@gmail.com>
2025-08-18 13:36:17 -0300

    Refatorando método create

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,5 +1,7 @@
 const { v4 } = require("uuid");
 
+const db = require("../../database");
+
 let contacts = [
   {
     id: v4(),
@@ -36,28 +38,26 @@ class ContactsRepository {
     );
   }
 
-    delete(id) {
+  delete(id) {
     return new Promise((resolve) => {
       contacts = contacts.filter((contact) => contact.id !== id);
       resolve();
     });
   }
 
-  create({name, email, phone, category_id}) {
-    return new Promise((resolve) => {
-      const newContact = {
-        id: v4(),
-        name,
-        email,
-        phone,
-        category_id,
-      }
-      contacts.push(newContact)
-      resolve(newContact);
-    });
+  async create({ name, email, phone, category_id }) {
+    const [row] = await db.query(
+      `INSERT INTO contacts(name, email, phone, category_id)
+      VALUES($1, $2, $3, $4)
+      RETURNING *
+      `,
+      [name, email, phone, category_id]
+    );
+
+    return row;
   }
 
-    update( id, {name, email, phone, category_id}) {
+  update(id, { name, email, phone, category_id }) {
     return new Promise((resolve) => {
       const updatedContact = {
         id,
@@ -67,11 +67,11 @@ class ContactsRepository {
         category_id,
       };
 
-      contacts = contacts.map((contact) => (
+      contacts = contacts.map((contact) =>
         contact.id === id ? updatedContact : contact
-      ))
+      );
 
-      resolve(updatedContact)
+      resolve(updatedContact);
     });
   }
 }
